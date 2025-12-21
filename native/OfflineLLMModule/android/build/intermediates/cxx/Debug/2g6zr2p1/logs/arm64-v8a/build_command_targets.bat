6880b24f1d7bc8aed7c246ed79e2ec7f6ac218f8
Vivekanand <rksuccessor@gmail.com>
2025-12-21 16:31:28 +0530

    commit fixes

--- native/OfflineLLMModule/android/build/intermediates/cxx/Debug/2g6zr2p1/logs/arm64-v8a/build_command_targets.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-"C:\\Users\\birba\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Vivekanand folder\\offline-ai-app\\native\\OfflineLLMModule\\android\\.cxx\\Debug\\2g6zr2p1\\arm64-v8a" ^
-  ggml ^
-  ggml-base ^
-  ggml-cpu ^
-  llama ^
-  offlinellm
--- native/OfflineLLMModule/android/build/intermediates/cxx/Debug/2g6zr2p1/logs/x86_64/build_command_targets.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-"C:\\Users\\birba\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Vivekanand folder\\offline-ai-app\\native\\OfflineLLMModule\\android\\.cxx\\Debug\\2g6zr2p1\\x86_64" ^
-  ggml ^
-  ggml-base ^
-  ggml-cpu ^
-  llama ^
-  offlinellm
